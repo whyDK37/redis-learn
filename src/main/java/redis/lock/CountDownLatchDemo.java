5f74da6127c27a28c7dcdcf3bda2527c52f7a990
djwanghuanyu <CFSGuN#7>
2019-09-15 20:22:30 +0800

    demo

---
+++ src/main/java/redis/lock/CountDownLatchDemo.java
@@ -0,0 +1,23 @@
+package redis.lock;
+
+import org.redisson.Redisson;
+import org.redisson.api.RCountDownLatch;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+public class CountDownLatchDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+
+// Sync and Async API
+        final RedissonClient redisson = Redisson.create(config);
+        final RCountDownLatch latch = redisson.getCountDownLatch("anyCountDownLatch");
+        latch.trySetCount(1);
+
+        latch.await();
+        System.out.println("start to run");
+
+    }
+}
