dad99236d81fe14ca5017b19e7b7db307a1d0726
djwanghuanyu <CFSGuN#7>
2019-09-15 20:02:49 +0800

    init

---
+++ src/main/java/redis/RedisDemo.java
@@ -0,0 +1,36 @@
+package redis;
+
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.api.RedissonRxClient;
+import org.redisson.config.Config;
+
+import java.util.concurrent.TimeUnit;
+
+public class RedisDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+
+// Sync and Async API
+        RedissonClient redisson = Redisson.create(config);
+
+        RLock lock = redisson.getLock("anyLock");
+// Most familiar locking method
+        lock.lock();
+        lock.lock();
+        System.out.println("locking...");
+        TimeUnit.SECONDS.sleep(30L);
+        lock.unlock();
+        System.out.println(lock.isLocked());
+
+// Reactive API
+        RedissonReactiveClient redissonReactive = Redisson.createReactive(config);
+
+// RxJava2 API
+        RedissonRxClient redissonRx = Redisson.createRx(config);
+    }
+}
